5b6189055cafbcd040c630bebda436c49ff8a7ce
Parham <parhamnooranbakht@gmail.com>
2020-07-24 22:49:44 +0430

    UML Diagram added

--- src/main/java/ir/ac/kntu/menu/TypeOfPlayer.java
+++ src/main/java/ir/ac/kntu/menu/TypeOfPlayer.java
@@ -3,6 +3,7 @@ package ir.ac.kntu.menu;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 
@@ -27,17 +28,11 @@ public class TypeOfPlayer {
         setBackStatus();
         setSpinnersStatus();
     }
-    private void setPlayers(){
-
-    }
 
     private void setSpinnersStatus(){
         for(int i=0;i<players.getDirector().getPlayers().size();i++) {
             Spinner<String> spinner = new Spinner<>();
-            spinner.setMinSize(100, 50);
-            spinner.getStyleClass().add(Spinner.STYLE_CLASS_ARROWS_ON_RIGHT_HORIZONTAL);
-            spinner.setValueFactory(new SpinnerValueFactory.ListSpinnerValueFactory<>(observableList));
-            spinner.setLayoutX(300);
+            setALabel(i);
             setSpinnerStatus(spinner, i);
             setOnSpinnerAction(spinner, i);
             types.add(spinner);
@@ -47,8 +42,18 @@ public class TypeOfPlayer {
         spinner.valueProperty().addListener((observableValue, oldValue, newValue) ->
                 players.getDirector().getPlayers().get(i).setControl(Control.valueOf(newValue.toUpperCase())));
     }
+    private void setALabel(int i){
+        Label label = new Label(players.getDirector().getPlayers().get(i).getName());
+        label.relocate(200, 60+50*i);
+        players.getDirector().getPane().getChildren().add(label);
+    }
+
     private void setSpinnerStatus(Spinner<String> spinner, int i){
+        spinner.setMinSize(100, 50);
+        spinner.setLayoutX(300);
         spinner.setLayoutY(40+50*i);
+        spinner.setValueFactory(new SpinnerValueFactory.ListSpinnerValueFactory<>(observableList));
+        spinner.getStyleClass().add(Spinner.STYLE_CLASS_ARROWS_ON_RIGHT_HORIZONTAL);
     }
     private void setButtonStatus(){
         button.setMinSize(150,50);
